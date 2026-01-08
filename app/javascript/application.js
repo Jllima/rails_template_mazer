c929636ed30d4869f6942fc12801867ea67e036c
Jorge <jlimajorge@gmail.com>
2026-01-08 14:11:59 -0300

    adiconando assets

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,14 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import featherIcons from "feather-icons"
+import "./mazer"
+import "./initTheme"
+
+// jQuery precisa vir antes do Parsley
+// expoerta jQuery para o escopo global
+import $ from "jquery"
+window.$ = $
+window.jQuery = $
+
+featherIcons.replace()
